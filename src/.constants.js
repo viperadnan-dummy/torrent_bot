64ad5b53fff8b5e3c2dceb62ae7cbab2f53f1de3
viperadnan-dummy <60770759+viperadnan-dummy@users.noreply.github.com>
2020-02-16 15:06:00 +0530

    Update .constants.js

--- src/.constants.js
+++ src/.constants.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-  TOKEN: '923360693:AAGv2TSf9oHnSRHqfp6e8vbtN0UbBYORFgs', //required
+  TOKEN: '1046682158:AAEJgnN_fiKrBqeG6WzpkZ1F7UCMJk_xe64', //required
   ARIA_SECRET: 'bhadoo', //change as per needs.
   ARIA_DOWNLOAD_LOCATION: '/app/downloads)', //-leave as it is
   INDEX_DOMAIN: 'https://drive.kltorrent.workers.dev/Torrent_bot/', // replace with your index domain. use https://github.com/ParveenBhadooOfficial/go-drive
@@ -7,7 +7,7 @@ module.exports = Object.freeze({
   ARIA_FILTERED_DOMAINS: [], // Prevent downloading from URLs containing these substrings -leave as it is
   ARIA_FILTERED_FILENAMES: [], // Files/top level directories with these substrings in the filename won't be downloaded -leave as it is
   ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well -leave as it is
-  GDRIVE_PARENT_DIR_ID:  '1Fj811b08_h9H3WjzBVJZBrqFUJgWLRdp', // for directorty eg. https://drive.google.com/drive/folders/1p7pHhSh2yboWePmo53DnVr3xkAYiojP9 use 1p7pHhSh2yboWePmo53DnVr3xkAYiojP9
+  GDRIVE_PARENT_DIR_ID:  '18PzcPMm1vpiXoHRMHtAuvTxFkuGSIKW_', // for directorty eg. https://drive.google.com/drive/folders/1p7pHhSh2yboWePmo53DnVr3xkAYiojP9 use 1p7pHhSh2yboWePmo53DnVr3xkAYiojP9
   SUDO_USERS: [335834367],	// Telegram user IDs. These users can use the bot in any chat. required, get it using https://t.me/YourTGid_Bot
   AUTHORIZED_CHATS: [-1001379500877],	// Telegram chat IDs. Anyone in these chats can use the bot. Add https://t.me/YourTGid_Bot to Group and click /id
   STATUS_UPDATE_INTERVAL_MS: 12000, // A smaller number will update faster, but might cause rate limiting -leave as it is
